e465c4a3dc9498c41d5620c9c766921cdcacfb83
pbiro03 <p.biro03@stud.uni-obuda.hu>
2023-10-22 20:52:00 +0200

    DBContext DbSets, ctor and OnConfiguring method

--- DPMHKV_HFT_2023241.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DPMHKV_HFT_2023241.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ DPMHKV_HFT_2023241.Repository/GuitarDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DPMHKV_HFT_2023241.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DPMHKV_HFT_2023241.Repository
+{
+    internal class GuitarDBContext:DbContext  
+    {
+        public DbSet<Guitar> Guitars { get; set; }
+        public DbSet<Musician> Musicians { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public GuitarDBContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           if (!optionsBuilder.IsConfigured)
+            {
+                string conn = @"";
+                optionsBuilder.UseLazyLoadingProxies().UseSqlServer(conn);
+            }
+        }
+    }
+}
