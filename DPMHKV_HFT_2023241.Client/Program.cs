bb7995cbcb8176f522d27a9bad474238c924a0a9
pbiro03 <p.biro03@stud.uni-obuda.hu>
2023-10-30 13:33:24 +0100

    IRepository interface and AbstractRepository class added

--- DPMHKV_HFT_2023241.Client/Program.cs
+++ DPMHKV_HFT_2023241.Client/Program.cs
@@ -8,8 +8,13 @@ namespace DPMHKV_HFT_2023241.Client
     {
         static void Main(string[] args)
         {
-            
-            
+            GuitarDBContext ctx = new GuitarDBContext();
+            var q1 = ctx.Guitars.Select(x => x.Color);
+            var q2 = ctx.Brands.Select(x => x.NetWorth);
+            var q3 = ctx.Musicians.Select(x => $"{x.Name} from {x.BandName}");
+
+            ;
+
         }
     }
 }
---
+++ DPMHKV_HFT_2023241.Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Castle.DynamicProxy.Generators.Emitters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPMHKV_HFT_2023241.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> ReadAll();
+        void Create(T item);
+        T Read(int id);
+        void Update(T item);
+        void Delete(int id);
+    }
+
+}
